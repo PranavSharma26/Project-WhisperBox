917f2015b1f9904b3aab4c16ee4aaa4b219c60c6
PranavSharma26 <pranavsharma12355@gmail.com>
2024-12-10 21:22:13 +0530

    Building User Dashboard

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -10,7 +10,7 @@ function Navbar() {
     return (
         <nav className='p-4 md:p-6 shadow-md'>
             <div className='container mx-auto flex flex-col sm:flex-row justify-between items-center'>
-                <a className='text-xl font-bold mb-4 md:mb-0' href='#'>Mystry Message</a>
+                <a className='text-xl font-bold mb-4 md:mb-0' href='#'>Mystery Message</a>
                 {
                     session ? (
                         <>                        <span className='mr-4'>Welcome, {user?.username || user?.email}</span>
