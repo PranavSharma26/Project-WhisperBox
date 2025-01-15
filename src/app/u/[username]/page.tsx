b0147d28bd59652bc9946c169c5df18d33bcfbdf
PranavSharma26 <pranavsharma12355@gmail.com>
2025-01-15 22:56:34 +0530

    user Message not showing problem fixed

--- src/app/u/[username]/page.tsx
+++ src/app/u/[username]/page.tsx
@@ -1,5 +1,4 @@
 'use client';
-
 import React, { useState } from 'react';
 import axios, { AxiosError } from 'axios';
 import { zodResolver } from '@hookform/resolvers/zod';
@@ -7,8 +6,6 @@ import { useForm } from 'react-hook-form';
 import { Loader2 } from 'lucide-react';
 import { Button } from '@/components/ui/button';
 import { Separator } from '@/components/ui/separator';
-// import { CardHeader, CardContent, Card } from '@/components/ui/card';
-// import { useCompletion } from 'ai/react';
 import {
   Form,
   FormControl,
@@ -24,40 +21,14 @@ import { ApiResponse } from '@/types/ApiResponse';
 import Link from 'next/link';
 import { useParams } from 'next/navigation';
 import { MessageSchema } from '@/schemas/messageSchema';
-
 const specialChar = '||';
-
-// const parseStringMessages = (messageString: string): string[] => {
-//   return messageString.split(specialChar);
-// };
-
-// const initialMessageString =
-  // "What's your favorite movie?||Do you have any pets?||What's your dream job?";
-
 export default function SendMessage() {
   const params = useParams<{ username: string }>();
   const username = params.username;
-
-  // const {
-  //   complete,
-  //   completion,
-  //   isLoading: isSuggestLoading,
-  //   error,
-  // } = useCompletion({
-  //   api: '/api/suggest-messages',
-  //   initialCompletion: initialMessageString,
-  // });
-
   const form = useForm<z.infer<typeof MessageSchema>>({
     resolver: zodResolver(MessageSchema),
   });
-
   const messageContent = form.watch('content');
-
-  // const handleMessageClick = (message: string) => {
-  //   form.setValue('content', message);
-  // };
-
   const [isLoading, setIsLoading] = useState(false);
 
   const onSubmit = async (data: z.infer<typeof MessageSchema>) => {
@@ -85,16 +56,6 @@ export default function SendMessage() {
       setIsLoading(false);
     }
   };
-
-  // const fetchSuggestedMessages = async () => {
-  //   try {
-  //     complete('');
-  //   } catch (error) {
-  //     console.error('Error fetching messages:', error);
-  //     // Handle error appropriately
-  //   }
-  // };
-
   return (
     <div className="container mx-auto my-8 p-6 bg-white rounded max-w-4xl">
       <h1 className="text-4xl font-bold mb-6 text-center">
@@ -133,40 +94,6 @@ export default function SendMessage() {
           </div>
         </form>
       </Form>
-
-      {/* <div className="space-y-4 my-8">
-        <div className="space-y-2">
-          <Button
-            onClick={fetchSuggestedMessages}
-            className="my-4"
-            disabled={isSuggestLoading}
-          >
-            Suggest Messages
-          </Button>
-          <p>Click on any message below to select it.</p>
-        </div>
-        <Card>
-          <CardHeader>
-            <h3 className="text-xl font-semibold">Messages</h3>
-          </CardHeader>
-          <CardContent className="flex flex-col space-y-4">
-            {error ? (
-              <p className="text-red-500">{error.message}</p>
-            ) : (
-              parseStringMessages(completion).map((message, index) => (
-                <Button
-                  key={index}
-                  variant="outline"
-                  className="mb-2"
-                  onClick={() => handleMessageClick(message)}
-                >
-                  {message}
-                </Button>
-              ))
-            )}
-          </CardContent>
-        </Card>
-      </div> */}
       <Separator className="my-6" />
       <div className="text-center">
         <div className="mb-4">Get Your Message Board</div>
--- src/components/MessageCard.tsx
+++ src/components/MessageCard.tsx
@@ -21,10 +21,11 @@ import {
 } from "@/components/ui/alert-dialog"
 import { Button } from './ui/button'
 import { X } from 'lucide-react'
+import dayjs from 'dayjs';
+import axios, { AxiosError } from 'axios';
 import { Message } from '@/model/User'
 import { useToast } from '@/hooks/use-toast'
 import { ApiResponse } from '@/types/ApiResponse'
-import axios from 'axios'
 
 type MessageCardProps={
 	message: Message;
@@ -34,40 +35,62 @@ type MessageCardProps={
 const MessageCard=({message, onMessageDelete}: MessageCardProps)=> {
 	const {toast} = useToast()
 	const handleDeleteConfirm = async ()=>{
-		const response = await axios.delete<ApiResponse>(`/api/delete-message/${message._id}`)
-		toast({
-			title: response.data.message
-		})
-		onMessageDelete(message._id)
+		try {
+			const response = await axios.delete<ApiResponse>(
+			  `/api/delete-message/${message._id}`
+			);
+			toast({
+			  title: response.data.message,
+			});
+			onMessageDelete(message._id);
+	  
+		  } catch (error) {
+			const axiosError = error as AxiosError<ApiResponse>;
+			toast({
+			  title: 'Error',
+			  description:
+				axiosError.response?.data.message ?? 'Failed to delete message',
+			  variant: 'destructive',
+			});
+		  } 
 	}
 
 	return (
-		<Card>
-			<CardHeader>
-				<CardTitle>Card Title</CardTitle>
-				<AlertDialog>
-					<AlertDialogTrigger asChild>
-						<Button variant="destructive"><X className='w-5 h-5'/></Button>
-					</AlertDialogTrigger>
-					<AlertDialogContent>
-						<AlertDialogHeader>
-							<AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
-							<AlertDialogDescription>
-								This action cannot be undone. This will permanently delete your
-								account and remove your data from our servers.
-							</AlertDialogDescription>
-						</AlertDialogHeader>
-						<AlertDialogFooter>
-							<AlertDialogCancel>Cancel</AlertDialogCancel>
-							<AlertDialogAction onClick={handleDeleteConfirm}>Continue</AlertDialogAction>
-						</AlertDialogFooter>
-					</AlertDialogContent>
-				</AlertDialog>
-				<CardDescription>Card Description</CardDescription>
-			</CardHeader>
-			<CardContent>
-			</CardContent>
-		</Card>
+		<Card className="card-bordered">
+      <CardHeader>
+        <div className="flex justify-between items-center">
+          <CardTitle>{message.content}</CardTitle>
+          <AlertDialog>
+            <AlertDialogTrigger asChild>
+              <Button variant='destructive'>
+                <X className="w-5 h-5" />
+              </Button>
+            </AlertDialogTrigger>
+            <AlertDialogContent>
+              <AlertDialogHeader>
+                <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
+                <AlertDialogDescription>
+                  This action cannot be undone. This will permanently delete
+                  this message.
+                </AlertDialogDescription>
+              </AlertDialogHeader>
+              <AlertDialogFooter>
+                <AlertDialogCancel>
+                  Cancel
+                </AlertDialogCancel>
+                <AlertDialogAction onClick={handleDeleteConfirm}>
+                  Continue
+                </AlertDialogAction>
+              </AlertDialogFooter>
+            </AlertDialogContent>
+          </AlertDialog>
+        </div>
+        <div className="text-sm">
+          {dayjs(message.createdAt).format('MMM D, YYYY h:mm A')}
+        </div>
+      </CardHeader>
+      <CardContent></CardContent>
+    </Card>
 	)
 }
 
